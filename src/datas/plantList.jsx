10936f0ee5f13b2a6a8428caad8ece3adcc12322
kevas007 <kevassiobo@gmail.com>
2021-07-03 12:54:14 +0200

    ajout du composants plantListes

---
+++ src/datas/plantList.jsx
@@ -0,0 +1,101 @@
+import monstera from '../image/monstera.jpg'
+import lyrata from '../image/lyrata.jpg'
+import pothos from '../image/pothos.jpg'
+import succulent from '../image/succulent.jpg'
+import olivier from '../image/olivier.jpg'
+import basil from '../image/basil.jpg'
+import mint from '../image/mint.jpg'
+import kev from '../image/kev.jpg'
+import calathea from '../image/calathea.jpg'
+import cactus from '../image/cactus.jpg'
+
+
+
+
+
+export const plantList =[
+
+    {
+        name: 'nucifera',
+        category: 'esotique',
+        id: '2eud',
+        light: 2,
+        water: 4,
+        cover: kev,
+        price: 75 + "€"
+    },
+    {
+        name: 'monstera',
+        category: 'classique',
+        id: '1ed',
+        bestsale:"true",
+        light: 2,
+        water: 3,
+        cover: monstera,
+        price: 15 + "€"
+    },
+    {
+        name: 'ficus lyrata',
+        category: 'classique',
+        id: '2ab',
+        light: 3,
+        water: 1,
+        cover: lyrata,
+        price: 16 + "€"
+    },
+    {
+        name: 'pothos argneté',
+        category: 'classique',
+        id: '3sd',
+        light: 2,
+        water: 4,
+        cover: pothos,
+        price: 9 + "€"
+    },
+    {
+        name: 'calathea',
+        category: 'classique',
+        id: '5pl',
+        light: "3",
+        water: "1",
+        cover: olivier,
+        price: 15 + "€"
+    },
+    {
+        name: 'cactus',
+        category: 'plante grasse',
+        id: '8fp',
+        light: 2,
+        water: 1,
+        cover: cactus,
+        price: 6 + "€"
+    },
+    {
+        name: 'basilique',
+        category: 'extérieur',
+        bestSale: true,
+        id: '7ie',
+        light: 2,
+        water: 3,
+        cover: basil,
+        price: 5 + "€"
+    },
+    {
+        name: 'succulente',
+        category: 'plznte grasse',
+        id: '9vn',
+        light: 2,
+        water: 1,
+        cover: succulent,
+        price: 8 + "€"
+    },
+    {
+        name: 'menthe',
+        category: 'exérieur',
+        id: '6uo',
+        light: 2,
+        water: 2,
+        cover: mint,
+        price: 4 + "€"
+    }
+];
