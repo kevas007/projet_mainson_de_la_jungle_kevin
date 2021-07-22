8ff8bb580c25e6f62bea0fdb4d98c207d56611cc
kevas007 <kevassiobo@gmail.com>
2021-07-22 09:17:45 +0200

    ajout de la class

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,12 +1,36 @@
-
+import React, {Component} from 'react';
 import { useHistory } from 'react-router-dom';
 import Banner from './Banner';
 import logo from '../image/logo.png';
 import'../styles/Home.css';
 import { Link } from "react-router-dom";
-function Home ()  {
-    let  change = useHistory();
+// function Home ()  {
+//     let  change = useHistory();
 
+//         return (
+//             <div>
+//                 <Banner>
+//                     <img  className="img-fluid lmj-logo"  src={logo} alt='logo-la-maison-jungle' />
+//                     <h1 className='lmj-title'>La maison jungle</h1>
+//                 </Banner>
+//                 <div className="container d-flex justify-content-center align-content-center ">
+//                     <div className="home mt-5 ">
+//                         <div className="d-flex  justify-content-center align-content-center">
+//                         <h1 className="text-success mb-5 ml-5 bton" > Bienvenue </h1>
+//                         <img  className="img-fluid images"  src={logo} alt='logo-la-maison-jungle' />
+                        
+//                         </div>
+//                         <button className="btn-success form-control w-50 " onClick={()=>{change.push("/Page2")} }>connexion</button>
+//                     </div>
+//                 </div>
+//             </div>
+//         );
+    
+// }
+
+// export default Home;
+export default class Home extends Component {
+    render() {
         return (
             <div>
                 <Banner>
@@ -15,17 +39,15 @@ function Home ()  {
                 </Banner>
                 <div className="container d-flex justify-content-center align-content-center ">
                     <div className="home mt-5 ">
-                        <div className="d-flex  justify-content-center align-content-center">
-                        <h1 className="text-success mb-5 ml-5 bton" > Bienvenue </h1>
-                        <img  className="img-fluid images"  src={logo} alt='logo-la-maison-jungle' />
-                        
+                        <div className="d-flex justify-content-center align-content-center">
+                            <h1 className="text-success mb-5 ml-5 bton" > Bienvenue </h1>
+                            <img  className="img-fluid images"  src={logo} alt='logo-la-maison-jungle' />
+                            
                         </div>
-                        <button className="btn-success form-control w-50 " onClick={()=>{change.push("/Page2")} }>connexion</button>
+                        <button className="btn-success form-control w-50 " onClick={()=>{this.props.history.push("/Page2")} }>connexion</button>
                     </div>
                 </div>
             </div>
-        );
-    
+        )
+    }
 }
-
-export default Home;
